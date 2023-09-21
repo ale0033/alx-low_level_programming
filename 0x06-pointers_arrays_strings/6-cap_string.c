de6374f5f819f59ad34e23e0f6ce0e0494df7254
SALEH ALNASAN <alnasan.ale@gmail.com>
2023-09-21 12:16:00 -0700

    A

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * is_separator - Checks if a character is a word separator
+ * @c: The character to check
+ *
+ * Return: 1 if c is a separator, 0 otherwise
+ */
+int is_separator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i]; i++)
+	{
+		if (c == separators[i])
+			return (1);
+	}
+
+	return (0);
+}
+
+char *cap_string(char *str)
+{
+	int i;
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] = str[0] - 32;
+	}
+
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if (is_separator(str[i - 1]) && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+
+	return (str);
+}
+
